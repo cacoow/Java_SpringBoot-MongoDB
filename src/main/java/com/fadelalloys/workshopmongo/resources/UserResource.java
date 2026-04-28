1e506a312c9157486ade5c2872769525247c6733
Caco <feeenergy1234@gmail.com>
2026-04-28 16:49:15 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/fadelalloys/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fadelalloys/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fadelalloys.workshopmongo.domain.Post;
 import com.fadelalloys.workshopmongo.domain.User;
 import com.fadelalloys.workshopmongo.dto.UserDTO;
 import com.fadelalloys.workshopmongo.services.UserService;
@@ -62,5 +63,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	// ou @GetMapping 
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
